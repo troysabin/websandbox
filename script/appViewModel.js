907342d8e080ffbe87144a5c617903a581e10d90
Troy Sabin <troy@troysabin.com>
2013-04-29 11:56:08 -0500

    Twitter

--- script/appViewModel.js
+++ script/appViewModel.js
@@ -1,8 +1,12 @@
 define(['knockout.min'], function(ko) {
+
     return function appViewModel() {
+    
         this.firstName = ko.observable('Bert');
+    
         this.firstNameCaps = ko.computed(function() {
             return this.firstName().toUpperCase();
         }, this);
+    
     };
 });
