2992039ca250b36f450064291b64fbf58fdca3c2
Troy Sabin <troy@troysabin.com>
2013-05-02 11:39:33 -0500

    SocialStream

--- SocialFeed/example/Twitter/js/TwitterModule.js
+++ SocialFeed/example/Twitter/js/TwitterModule.js
@@ -1,11 +1,31 @@
 // Twitter module
 ;(function (SocialFeed, root) {
 
+    String.prototype.parseTwitterURL = function() {
+    return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
+      return url.link(url);
+    });
+  };
+
+  String.prototype.parseTwitterUsername = function() {
+    return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+      var username = u.replace("@","")
+      return u.link("http://twitter.com/"+username);
+    });
+  };
+
+  String.prototype.parseTwitterHashtag = function() {
+    return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+      var tag = t.replace("#","%23")
+      return t.link("http://twitter.com/search?q="+tag);
+    });
+  };
+
   root.TwitterModule = SocialFeed.Modules.extend({
     url: function () {
       // Heroku host for node-serverside.js
       // ident is here is count.
-      return 'http://mib-twitfeed.herokuapp.com/?count=' + (this.ident || 10)
+      return 'http://troysabin.azurewebsites.net/?count=' + (this.ident || 10)
     }
 
     , orderBy: function (item) {
@@ -13,9 +33,10 @@
     }
 
     , render: function (item) {
-      var html = '<h2><a href="http://twitter.com/'+ item.user.screen_name+'">@'+item.user.screen_name+'</a> tweeted:</h2>';
-      html += '<p>' + item.text + '</p>';
-      return '<div class="socialfeed-item socialfeed-twitter"><i class="socialfeed-icon icon-twitter"></i>' + html + '</div>';
+      //var html = '<h2>@'+item.user.screen_name+' tweeted:</h2>';
+      var html = '<p>'+ item.text + '</p>';
+      html = '<div class="socialfeed-item socialfeed-twitter"><i class="socialfeed-icon icon-twitter"></i>' + html + '</div>';
+      return html.parseTwitterURL().parseTwitterUsername().parseTwitterHashtag();
     }
   });
 
